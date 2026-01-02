02f67ec1ffa349cb7c5a07cb9e8129969a53232c
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2026-01-02 12:51:00 +0200

    task 02 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+const ingredientListItem = ingredients.map((ingtedient) => {
+  const ingredientListItemEl = document.createElement("li");
+  ingredientListItemEl.textContent = ingtedient;
+  ingredientListItemEl.classList.add("item");
+  return ingredientListItemEl;
+});
+
+ingredientsList.append(...ingredientListItem);
