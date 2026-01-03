4ac928374fd621c78b0f923ea1de222ae453215d
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2026-01-03 11:37:01 +0200

    task 09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const widget = document.querySelector(".widget");
+const colorText = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", onBtnClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function onBtnClick() {
+  const randomHexColor = getRandomHexColor();
+  widget.style.backgroundColor = randomHexColor;
+  colorText.textContent = randomHexColor;
 }
