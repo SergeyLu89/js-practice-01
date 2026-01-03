abe684ade6a3a45722f3953c027bc2a06ba8d300
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2026-01-03 11:25:07 +0200

    task 08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const {
+    elements: { email, password },
+  } = evt.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("All fields must be filled!");
+  }
+  const userDtata = { email: email.value, password: password.value };
+  console.log("userDtata: ", userDtata);
+
+  evt.currentTarget.reset();
+}
